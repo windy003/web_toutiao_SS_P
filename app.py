1f41fe7a95f827b3fdf75120c7a36c70f2fb78de
windy003 <928920471@qq.com>
2025-09-07 18:52:47 +0800

    做了调整

--- app.py
+++ app.py
@@ -337,4 +337,4 @@ def serve_latest():
 if __name__ == '__main__':
     from waitress import serve
     serve(app, host='0.0.0.0', port=5008) 
-    # app.run(host='0.0.0.0', port=5008)
+    # app.run(debug=True,host='0.0.0.0', port=5008)
