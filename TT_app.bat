2cf35ee6d0828e005be3848726d5a1501601b2d7
windy003 <928920471@qq.com>
2026-01-15 18:46:20 +0800

    修改

--- TT_app.bat
+++ TT_app.bat
@@ -1,4 +1,4 @@
 
-cd /d "D:\files\using\web_toutiao_SS_P"   &&  start  pythonw "D:\files\using\web_toutiao_SS_P\app.py"
+cd /d "D:\files\using\Web\web_toutiao_SS_P"   &&  start  pythonw "D:\files\using\Web\web_toutiao_SS_P\app.py"
 
 
