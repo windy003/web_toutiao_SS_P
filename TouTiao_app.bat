21ae2ae8c8fd79b2c0aaf5af19536a7595cbd39b
windy003 <928920471@qq.com>
2026-02-12 07:14:58 +0800

    ok

--- TouTiao_app.bat
+++ TouTiao_app.bat
@@ -1,4 +1,4 @@
 
-cd /d "D:\files\using\Web\web_toutiao_SS_P"   &&  start  pythonw "D:\files\using\Web\web_toutiao_SS_P\app.py"
+cd /d "D:\files\using\Web\web_toutiao_SS_P"   &&  start /b pythonw "D:\files\using\Web\web_toutiao_SS_P\app.py"
 
 
